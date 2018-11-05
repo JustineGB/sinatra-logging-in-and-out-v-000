c2aaab2807ee109166d95c1da1b11d79da6a7f6f
Justine B <justinegayton@gmail.com>
2018-11-05 17:12:46 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -14,20 +14,8 @@ class ApplicationController < Sinatra::Base
   end
 
   post '/login' do
-    #@user = User.new(username: params["username"], password: params["password"])
-    #@user.save
-    #session[:username] = params[:username]
-    #session[:user_id] = @user.id
-    #(1) use `is_logged_in?` to display username & balance (if user logged-in)
-    #ELSE: link to the homepage
-    #(2) use current_user to show username and balance
     redirect '/account'
   end
 
-
-  #get '/logout' do
-  #  session.clear
-  #  redirect '/'
-  #end
 
 end
