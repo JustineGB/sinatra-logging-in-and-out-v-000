9e1bb618323bef0fc89e468ad6ec3f18f7c85f88
Justine B <justinegayton@gmail.com>
2018-11-05 18:23:21 +0000

    automatically backed up by learn

--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -2,13 +2,13 @@ require 'pry'
 
 class Helpers < ApplicationController
 
-  def is_logged_in?(session)
+  def self.is_logged_in?(session)
   #  if session[:usernam].empty?
   #    redirect "/index"
     !!session[:user_id]
   end
 
-  def current_user(session)
+  def self.current_user(session)
     session[:user_id] == @user.id
   end
 
