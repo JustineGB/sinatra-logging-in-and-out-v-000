0952987ff3e25b20c5c23520fc50bb58b5e1bfd0
Justine B <justinegayton@gmail.com>
2018-11-05 19:14:43 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -23,7 +23,7 @@ class ApplicationController < Sinatra::Base
   end
 
   get '/account' do
-    if is_logged_in?(session)
+    if @current_user 
       redirect '/account'
     else
       redirect '/error'
