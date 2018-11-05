6c69697894b3c3afb9ac5c208c9d840c94134255
Justine B <justinegayton@gmail.com>
2018-11-05 17:23:55 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -50,9 +50,9 @@ class ApplicationController < Sinatra::Base
     redirect '/'
   end
 
-  get 'account' do
-    @user = User.find(session[:user_id])
-    erb :'account'
-  end
-
+  #get 'account' do
+    #@user = User.find(session[:user_id])
+    #erb :'account'
+  #end
+  
 end
