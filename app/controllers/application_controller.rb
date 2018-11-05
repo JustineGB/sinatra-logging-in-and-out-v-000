a9bb4bbed07a3c05cb9558fb0c71c9816263ec0e
Justine B <justinegayton@gmail.com>
2018-11-05 16:23:12 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -14,7 +14,7 @@ class ApplicationController < Sinatra::Base
   end
 
   post '/login' do
-    raise params.inspect
+    #raise params.inspect
     @user = User.new(username: params["username"], password: params["password"])
     @user.save
     session[:user_id] = @user.id
