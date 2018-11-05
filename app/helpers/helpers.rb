7fef59534b1edc82c0980d2e9b477fae778c8658
Justine B <justinegayton@gmail.com>
2018-11-05 19:07:17 +0000

    automatically backed up by learn

--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -9,7 +9,7 @@ class Helpers < ApplicationController
   end
 
   def self.current_user(session)
-    @current_user || = User.find_by(:id => session[:user_id]) if session[:user_id]
+    @current_user ||= User.find_by(:id => session[:user_id]) if session[:user_id]
   end
 
   def logout
