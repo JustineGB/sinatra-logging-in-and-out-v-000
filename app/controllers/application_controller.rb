ac0fceea4e2ad95e140f94ea4c4edd54b8e6d1b5
Justine B <justinegayton@gmail.com>
2018-11-05 18:06:38 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -14,13 +14,7 @@ class ApplicationController < Sinatra::Base
   end
 
   post '/login' do
-    #binding.pry
-    #user would have already been created so this is not a new user (not a 'registration' like the previous lab, just a log in...use find+by to find the user as their identity name/pass are already set up)
-    #@user = User.new(username: params["username"], password: params["password"])
-    #@user.save
-    #session[:username] = params[:username]
-
-    #login(params[:username])
+    
     @user = User.find_by(username: params["username"], password: params["password"])
     session[:user_id] = @user.id
       if @user.is_logged_in?
